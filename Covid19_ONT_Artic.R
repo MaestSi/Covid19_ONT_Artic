18c08e0f88cc0fa40a7fcc9dd50d68c81e22a3d5
Simone Maestri <simone.maestri@hotmail.it>
2021-04-03 13:38:42 +0200

    Update Covid19_ONT_Artic.R

--- Covid19_ONT_Artic.R
+++ Covid19_ONT_Artic.R
@@ -356,7 +356,7 @@ for (i in 1:length(BC_files)) {
    theme(plot.title=element_text(size=20, hjust = 0.5)) +
    scale_y_continuous(breaks = c(0, 1, 2, 3, 4, 5), labels=c("0","10","100","1000","10000","100000"), limits=c(0, 5)) +
    scale_x_discrete(name = "Amplicon", limits = as.character(coverage_data$Amplicon)) +
-   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), axis.text.y=element_text(size=30))
+   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), axis.text.y=element_text(size=20))
   
    ggsave(
    paste0(d3, "/", sample_name_curr, "_PrimerCov.png"),
